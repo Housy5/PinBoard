e4419b9294d0622429b4a4fb45cf92a1b976860d
Housy5 <50631944+Housy5@users.noreply.github.com>
2023-01-30 19:00:50 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,88 @@
+import random
+
+class Pin:
+    
+    _used_ids: list[str] = []
+    
+    def _random_char(self) -> str:
+        return hex(random.randint(0, 0xf))[2:]
+    
+    def _generate_id(self) -> str:
+        chars = []
+        length: int = 4
+        while length > 0:
+            length -= 1
+            chars.append(self._random_char())
+        return ''.join(chars)
+    
+    def _to_sentence_case(self, data: str) -> str:
+        if len(data) == 1:
+            return data.upper()
+        return data[0].upper() + data[1:]
+    
+    def _init_id(self):
+        while (id := self._generate_id()) in self._used_ids:
+            pass
+        self._id = id
+        self._used_ids.append(id)
+        
+    def __init__(self, message: str):
+        self._init_id()
+        self._msg = self._to_sentence_case(message)
+    
+    def get_message(self) -> str:
+        return self._msg
+    
+    def get_id(self) -> str:
+        return self._id
+    
+    def __str__(self) -> str:
+        return f"Message: {self.get_message()}, ID: {self.get_id()}"
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Pin):
+            return False
+        return self.get_id() == other.get_id() and self.get_message() == other.get_message()
+
+    def __delete__(self):
+        index = self._used_ids.index(self.get_id())
+        del(self._used_ids[index])
+
+class NoSuchIDException(Exception):
+    pass
+
+class PinBoard():
+    
+    def __init__(self):
+        self.board: list[Pin] = []
+    
+    def add_pin(self, pin: Pin) -> bool:
+        if pin in self.board:
+            return False
+        self.board.append(pin)
+        return True
+
+    def find_index_for(self, pin: Pin) -> int:
+        result = -1
+        for cursor in range(len(self.board)):
+            x = self.board[cursor]
+            if x == pin:
+                result = cursor
+                break
+        return result
+
+    def find_pin_by_id(self, id: str) -> Pin:
+        for x in self.board:
+            if x.get_id() == id:
+                return x
+        raise NoSuchIDException("There is no pin with the specified ID!")
+
+    def remove_pin(self, pin: Pin) -> bool:
+        if pin not in self.board:
+            return False
+        index: int = self.find_index_for(pin)
+        if index == -1:
+            return False
+        del(self.board[index])
+        return True
+
---
+++ pinner.py
@@ -0,0 +1,112 @@
+from models import Pin
+from models import PinBoard
+from models import NoSuchIDException
+
+import os
+import pickle
+
+board = None
+push_file_name = "pins.pin"
+settings_file_name = "settings.pin"
+auto_push = True
+running = True
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def save_settings():
+    file = open(settings_file_name, "w")
+    file.write('1' if auto_push else '0')
+    file.close()
+
+def load_settings():
+    global auto_push
+    if not os.path.isfile(settings_file_name):
+        return
+    file = open(settings_file_name, "r")
+    auto_push = True if file.read() == '1' else False
+    file.close()
+
+def push_to_file():
+    file = open(push_file_name, "wb")
+    pickle.dump(board, file)
+    file.close()
+    print("Push successful.")
+
+def load_from_file():
+    global board
+    
+    if not os.path.isfile(push_file_name):
+        board = PinBoard()
+        return
+    
+    file = open(push_file_name, 'rb')
+    board = pickle.load(file)
+    file.close()
+
+def pin(msg: str):
+    new_pin = Pin(msg)
+    if not board.add_pin(new_pin):
+        print('Failed to add a new pin.')
+        return
+    print("Successfully added a new pin!")
+
+def unpin(id: str):
+    try:
+        pin: Pin = board.find_pin_by_id(id)
+        board.remove_pin(pin)
+    except NoSuchIDException as e:
+        print(e)
+    
+def print_pins():
+    pins: list[Pin] = board.board
+    for x in pins:
+        print(f"ID: {x.get_id()}\tMessage: {x.get_message()}.")
+
+def print_help():
+    print("All Commands: ")
+    print("\thelp - Shows this text.")
+    print("\tcls  - Clears the console.")
+    print("\tpin [text] - Adds a new pin.")
+    print("\tunpin [ID] - Removes an existing pin.")
+    print("\texit - Exits the program.")
+    print("\tauto_push - Toggles automatic saving.")
+    print("\tpush - Saves the current state of the program.")
+
+def main():
+    global running
+    global auto_push
+    
+    user_input: str = input(' >> ').casefold()
+    if user_input.startswith('pin '):
+        pin(user_input[4:])
+        if (auto_push):
+            push_to_file()
+    elif user_input == 'pins':
+        print_pins()
+    elif user_input.startswith('unpin '):
+        unpin(user_input[6:])
+        if (auto_push):
+            push_to_file()
+    elif user_input == 'cls':
+        clear_console()
+    elif user_input == 'exit':
+        running = False
+    elif user_input == 'push':
+        push_to_file()
+    elif user_input == 'auto_push':
+        auto_push = not auto_push
+        msg = "Auto push enabled" if auto_push else "Auto push disabled."
+        print(msg)
+        save_settings()
+    elif user_input == 'help':
+        print_help()
+
+def static_init():
+    load_from_file()
+
+if __name__ ==  '__main__':
+    static_init()
+    load_settings()
+    while (running):
+        main()
